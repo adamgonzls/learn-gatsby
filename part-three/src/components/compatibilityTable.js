4d39b849f9245856f1b81e63d2e6db960be3d126
Adam Gonzales <adamgonzales1@gmail.com>
2019-09-10 22:14:06 -0700

    add exercise files

---
+++ part-three/src/components/compatibilityTable.js
@@ -0,0 +1,202 @@
+import React from "react"
+export default () => (
+  <table className="compatibility-table">
+    <thead>
+      <tr>
+        <th colSpan="2" width="40%"> </th>
+        <th width="15%">Apple iOS</th>
+        <th width="15%">Android</th>
+        <th width="15%">macOS</th>
+        <th width="15%">Windows</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr className="compatibility-feature-section-title">
+        <th colSpan="6">Compatibility</th>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Minimum OS</td>
+        <td className="compatx">8</td>
+        <td className="compatx">4.0.3 <span className="compatibility-notes-text">Ice Cream Sandwich MR1</span></td>
+        <td className="compatx">10.10 <span className="compatibility-notes-text">Yosemite</span></td>
+        <td className="compatx">7</td>
+      </tr>
+    </tbody>
+    <tbody>
+      <tr className="compatibility-feature-section-title">
+        <th colSpan="6">Basic</th>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Connect via WiFi</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Compose Images with Live View</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Tap/Click to focus on Live View</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Control Camera Settings</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Save Camera Settings</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Bracketing</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Bulb Timer</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Focus Stacking</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">HDR</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Time-Lapse</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Capture Video</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Download JPG</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Download RAW Files</td>
+        <td className="compatx">Y - iOS 10.0+</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Magnify in Live View</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Live Histogram</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Focus Peaking</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Pinch to Zoom on Photo Review</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Set Download Location</td>
+        <td></td>
+        <td></td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Mirror Preview</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Auto/Manual Focus</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Grid</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Single Shot/Continuous Shot</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">EXIF</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td className="compatibility-feature-name" colSpan="2">Social Share*</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+        <td className="compatx">Y</td>
+      </tr>
+      <tr className="compatibility-notes">
+        <td className="compatibility-notes-text" colSpan="6">*Download RAW or JPG to device, use photo gallery/app to share</td>
+      </tr>
+    </tbody>
+  </table>
+)
